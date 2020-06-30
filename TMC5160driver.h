d0cf18e450300501bd24baf778c750b87b769a0d
unknown <zekeincapas@gmail.com>
2020-06-30 12:45:09 +1200

    Added registers we want to edit for stealthchop

--- TMC5160driver.h
+++ TMC5160driver.h
@@ -37,6 +37,15 @@ class TMC5160
     void hstrt(uint32_t* buffer); //3 bit: Hysteresis start value added to hend
     void toff(uint32_t* buffer); //4 bit: Off time setting control duration of slow decay phase
 
+    //PWMCONF
+    void pwm_lim(uint32_t* buffer); //4 bit: Reduce current jerk during mode change back to stealtchop from spreadcycle. set to 12.
+    void pwm_reg(uint32_t* buffer); //4 bit: Regulation loop gradient set to 4
+    void freewheel(uint32_t* buffer); //2 bit: 0: normal operation, 1: frewheeling
+    void pwm_autograd(uint32_t* buffer); //1 bit: Automatic gradient adaptation 0: Fixed value for PWM_GRAD, 1: Automatic tuning
+    void pwm_autoscale(uint32_t* buffer); //1 bit: PWM automatic amplitude scaling 0: User define PWM amplitude  1: Enable automatic current control
+    void pwm_freq1(uint32_t* buffer); //2 bit: Set to 1
+    void pwm_grad(uint32_t* buffer); //8 bit: Initialize user defined amplitude gradient; Set to 25
+    void pwm_ofs(uint32_t* buffer); //8 bits: Initialize user defined amplited offset; Set to 30
     
 
 
